478702f4b464476afcfaabdd30916da3ae68460f
phLuteRyuto <ryutonakamura1246@gmail.com>
2020-01-04 21:37:11 +0900

    スライダー廃止

--- index.js
+++ index.js
@@ -6,4 +6,30 @@
       document.body.classList.toggle('active');
     });
   });
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    spaceBetween: 0,
+    effect: 'fade',
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    loop: true,
+    // on:でコールバック関係
+    on: {
+      // スライドが変わった後のコールバック
+      slideChange: function () {
+        // thisの中にいろんなスライダー情報
+        console.log(this);
+        // スライドのdom情報（配列になってる）
+        console.log(this.slides);
+        // 今のスライドが何枚目か数字で返す
+        console.log(this.realIndex);
+      }
+    }
+  });
 })();
